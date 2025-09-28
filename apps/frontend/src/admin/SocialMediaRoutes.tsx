fa0212afce3258f8326e2cbe782f78064bea5f04
SINA PANAHI <93608711+S1NAPANAHI@users.noreply.github.com>
2025-09-29 01:00:52 +0200

    Update routes to use enhanced Social Media Generator

--- apps/frontend/src/admin/SocialMediaRoutes.tsx
+++ apps/frontend/src/admin/SocialMediaRoutes.tsx
@@ -1,12 +1,14 @@
 import React from 'react';
 import { Routes, Route } from 'react-router-dom';
+import { EnhancedSocialMediaGenerator } from './components/EnhancedSocialMediaGenerator';
 import { SocialMediaGenerator } from './components/SocialMediaGenerator';
 
 export const SocialMediaRoutes: React.FC = () => {
   return (
     <Routes>
-      <Route index element={<SocialMediaGenerator />} />
-      <Route path="generator" element={<SocialMediaGenerator />} />
+      <Route index element={<EnhancedSocialMediaGenerator />} />
+      <Route path="generator" element={<EnhancedSocialMediaGenerator />} />
+      <Route path="basic" element={<SocialMediaGenerator />} />
     </Routes>
   );
 };

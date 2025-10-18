98548acc82c0d5ae860ef743b1570ccf375697d5
SINA PANAHI <93608711+S1NAPANAHI@users.noreply.github.com>
2025-10-18 23:25:39 +0200

    chore: prepare SupportPage to render KoFiWidget alongside other payment methods

---
+++ apps/frontend/src/pages/support/_imports_patch.ts
@@ -0,0 +1,5 @@
+import React from 'react';
+import KoFiWidget from '../components/KoFiWidget';
+import BuyMeACoffeeWidget from '../components/BuyMeACoffeeWidget';
+import PayPalDonationButton from '../components/PayPalDonationButton';
+// ...rest of imports
